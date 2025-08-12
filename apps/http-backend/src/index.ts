8c3ccd77b588456c4098649a1c847e1715618ef9
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-08-12 17:58:43 +0530

    Added common package for zod and jwt config

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,15 +1,30 @@
 import express, { response } from "express";
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
 import { middleware } from "./middleware";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { CreateRoomSchema, CreateUserSchema, SignInSchema } from "@repo/common/types";
 
 const app = express()
 
 app.post("/signup", (req, res)=>{
-
+    const data = CreateUserSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
 })
 
 app.post("/signin", (req, res)=>{
+
+    const data = SignInSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
 
     const userId = 1;
     const token  = jwt.sign({
@@ -22,6 +37,13 @@ app.post("/signin", (req, res)=>{
 })
 
 app.post("/room",middleware, (req, res)=>{
+    const data = CreateRoomSchema.safeParse(req.body)
+    if(!data.success){
+        return res.json({
+            message: "Incorrect inputs"
+        })
+        return
+    }
 
 })
 
