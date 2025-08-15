051a9830798989987f3f0bad4d6d658680f93f94
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-08-15 12:42:47 +0530

    Added signup endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -7,21 +7,36 @@ import {prismaClient} from "@repo/db/client"
 
 const app = express()
 
-app.post("/signup", (req, res)=>{
-    const data = CreateUserSchema.safeParse(req.body)
-    if(!data.success){
+app.post("/signup", async (req, res)=>{
+    const parsedData = CreateUserSchema.safeParse(req.body)
+    if(!parsedData.success){
         return res.json({
             message: "Incorrect inputs"
         })
         return
     }
-    
+    try {
+        await prismaClient.user.create({
+        data:{
+            email: parsedData.data?.email,
+            password: parsedData.data?.password,
+            name: parsedData.data?.name
+        }
+    })
+    res.json({
+        userId: "123"
+    })
+    } catch (error) {
+        res.status(411).json({
+            message: "User already exist with this username"
+        })
+    }
 })
 
 app.post("/signin", (req, res)=>{
 
-    const data = SignInSchema.safeParse(req.body)
-    if(!data.success){
+    const parsedData = SignInSchema.safeParse(req.body)
+    if(!parsedData.success){
         return res.json({
             message: "Incorrect inputs"
         })
